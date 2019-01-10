013fb0133f72d80ac589ec6e0bc724a34ae3dc60
Jayesh Salvi <salvijayesh30@gmail.com>
2019-01-10 13:21:47 +0530

    Code file created.

---
+++ insurance-claim-prediction/code.py
@@ -0,0 +1,96 @@
+# --------------
+# import the libraries
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+import warnings
+warnings.filterwarnings('ignore')
+
+# Code starts here
+df = pd.read_csv(path)
+print(df.head())
+X = df.drop('insuranceclaim',axis=1)
+y = df.insuranceclaim
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=6)
+
+
+# Code ends here
+
+
+# --------------
+import matplotlib.pyplot as plt
+
+
+# Code starts here
+X_train['bmi'].plot(kind='box')
+q_value = np.quantile(X_train['bmi'],q=0.95)
+print(y_train.value_counts())
+
+# Code ends here
+
+
+# --------------
+# Code starts here
+
+relation  = X_train.corr()
+print(relation)
+sns.pairplot(X_train)
+# Code ends here
+
+
+# --------------
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Code starts here
+cols = ['children','sex','region','smoker']
+fig,axes = plt.subplots(nrows=2,ncols=2,figsize=(8,8))
+
+for i in range(2):
+    for j in range(2):
+        col = cols[i*2+j]
+        sns.countplot(x=X_train[col],hue=y_train,ax=axes[i,j])
+
+plt.show()
+
+
+# Code ends here
+
+
+# --------------
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+
+# parameters for grid search
+parameters = {'C':[0.1,0.5,1,5]}
+
+# Code starts here
+lr = LogisticRegression()
+grid = GridSearchCV(lr,param_grid=parameters)
+grid.fit(X_train,y_train)
+y_pred = grid.predict(X_test)
+accuracy = accuracy_score(y_test,y_pred)
+# Code ends here
+
+
+# --------------
+from sklearn.metrics import roc_auc_score,auc
+from sklearn import metrics
+
+# Code starts here
+
+score = roc_auc_score(y_pred,y_test)
+print(score)
+y_pred_proba = grid.predict_proba(X_test)[:,1]
+print(y_pred_proba)
+
+fpr,tpr,th= metrics.roc_curve(y_test,y_pred_proba)
+roc_auc = roc_auc_score(y_test,y_pred_proba)
+print(roc_auc)
+plt.plot(fpr,tpr,label="Logistic model, auc="+str(roc_auc))
+# Code ends here
+
+
